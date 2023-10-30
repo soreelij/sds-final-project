d67f44409d206f8ff5df95eedd081d666aa1a668
Elijah Sorensen <57324970+soreelij@users.noreply.github.com>
2023-10-30 16:25:11 -0600

    Code completion working!

---
+++ vscode/helloworld/src/extension.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import axios from 'axios';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const command = 'llama.codeCompletion';
+	const editor = vscode.window.activeTextEditor;
+
+	const selection = editor?.selection
+
+	const url = 'http://localhost:8000/v1/engines/copilot-codex/completions';
+
+	const commandHandler = () => {
+
+		if (selection && !selection.isEmpty) {
+
+			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+			const highlighted = editor.document.getText(selectionRange);
+
+			console.log("Requesting completion for " + highlighted);
+
+			axios.post(url, {
+				"prompt": highlighted,
+				"stop": ["\n\n"],
+				"max_tokens": 100,
+				"temperature": 0
+			}).then(function (response) {
+				const suggestion = response.data.choices[0].text
+				console.log(suggestion);
+				editor.edit(editBuilder => {
+					editBuilder.insert(new vscode.Position(selection.end.line, selection.end.character), suggestion)
+				})
+			}).catch(function (error) {
+				console.log(error);
+			});
+		}
+	};
+
+	context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+
+}
+
+export function deactivate() { }
